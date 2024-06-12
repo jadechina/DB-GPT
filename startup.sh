1c22460fc73d6021a7837aefbb97e501d63edc29
shentao <shentao@hailiang.com>
2024-06-12 16:41:23 +0800

    d

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
-conda activate dbgpt
+#conda init
+#conda activate dbgpt
 pid=$(ps -ef | grep dbgpt_server | grep -v grep | awk '{print $2}')
 if [ -n "$pid" ]; then
   /usr/bin/kill -9 $pid && sleep 5
